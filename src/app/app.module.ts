f63efa14f32ac187853b346efda3a14c0917d954
basavarajms03 <basavaraj.sangur03@outlook.com>
2021-05-03 21:54:58 +0530

    4 files modified, 14 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { CourseListComponent } from "./course-list/course-list.component";
+import { CourseEditComponent } from "./course-list/course-edit/course-edit.component";
+import { StudentsListComponent } from "./students-list/students-list.component";
+import { StudentsEditComponent } from "./students-list/students-edit/students-edit.component";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    CourseListComponent,
+    CourseEditComponent,
+    StudentsListComponent,
+    StudentsEditComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
